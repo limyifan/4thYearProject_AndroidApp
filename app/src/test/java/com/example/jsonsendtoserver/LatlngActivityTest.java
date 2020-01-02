6389aa6efd1e3939f3de0c902cac32d145192d54
charles <limyifan1997@gmail.com>
2020-01-02 18:59:06 +0000

    created one test file

---
+++ app/src/test/java/com/example/jsonsendtoserver/LatlngActivityTest.java
@@ -0,0 +1,28 @@
+package com.example.jsonsendtoserver;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LatlngActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onStart() {
+    }
+
+    @Test
+    public void onResume() {
+    }
+
+    @Test
+    public void onPause() {
+    }
+
+    @Test
+    public void onLocationChanged() {
+    }
+}
